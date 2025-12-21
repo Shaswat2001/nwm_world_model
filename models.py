14b953e1ba9f34c5b0d0dd57f5b5265b1849414d
Shaswat2001 <sis_shaswat@outlook.com>
2025-12-21 15:19:54 -0500

    fix: take the diffusion model from the original implementation for now, will create a custom one from scratch in the next iteration

---
+++ models.py
@@ -0,0 +1,221 @@
+import math
+import torch
+import torch.nn as nn
+import numpy as np
+from timm.models.vision_transformer import PatchEmbed, Attention, Mlp
+
+def modulate(x, shift, scale):
+    return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
+
+
+class TimeStepEmbedder(nn.Module):
+
+    def __init__(self, hidden_size: int, frequency_emb_size: int):
+
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_emb_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+
+        self.frequency_emb_size = frequency_emb_size
+
+    def time_embedding(self, time, frequency_dim, max_period=10000):
+
+        half = frequency_dim // 2
+        frequency = torch.exp(-math.log(max_period) * torch.arange(start= 0, end= half, dtype= torch.float32) / half).to(device = time.device)
+        param = time.float() * frequency[None]
+        embedding = torch.cat([torch.cos(param), torch.sin(param)], axis=-1)
+
+        if frequency_dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+
+        return embedding
+
+    def forward(self, time):
+
+        time_frequency = self.time_embedding(time, self.frequency_emb_size)
+        time_embedding = self.mlp(time_frequency)
+
+        return time_embedding
+    
+class ActionStepEmbedder(nn.Module):
+
+    def __init__(self, hidden_size: int, frequency_emb_size: int):
+
+        dim = hidden_size // 3
+        self.x_embedder = TimeStepEmbedder(dim, frequency_emb_size)
+        self.y_embedder = TimeStepEmbedder(dim, frequency_emb_size)
+        self.yaw_embedder = TimeStepEmbedder(dim, frequency_emb_size)
+
+    def forward(self, action):
+
+        return torch.cat([
+            self.x_embedder(action[...,0:1]),
+            self.y_embedder(action[...,1:2]),
+            self.yaw_embedder(action[...,2:3])
+        ], dim=-1)
+    
+class CDiTBlock(nn.Module):
+    """
+    A DiT block with adaptive layer norm zero (adaLN-Zero) conditioning.
+    """
+    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0, **block_kwargs):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.attn = Attention(hidden_size, num_heads=num_heads, qkv_bias=True, **block_kwargs)
+        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.norm_cond = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.cttn = nn.MultiheadAttention(hidden_size, num_heads=num_heads, add_bias_kv=True, bias=True, batch_first=True, **block_kwargs)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 11 * hidden_size, bias=True)
+        )
+
+        self.norm3 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        mlp_hidden_dim = int(hidden_size * mlp_ratio)
+        approx_gelu = lambda: nn.GELU(approximate="tanh")
+        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
+
+    def forward(self, x, c, x_cond):
+        shift_msa, scale_msa, gate_msa, shift_ca_xcond, scale_ca_xcond, shift_ca_x, scale_ca_x, gate_ca_x, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(11, dim=1)
+        x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa))
+        x_cond_norm = modulate(self.norm_cond(x_cond), shift_ca_xcond, scale_ca_xcond)
+        x = x + gate_ca_x.unsqueeze(1) * self.cttn(query=modulate(self.norm2(x), shift_ca_x, scale_ca_x), key=x_cond_norm, value=x_cond_norm, need_weights=False)[0]
+        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm3(x), shift_mlp, scale_mlp))
+        return x
+
+class FinalLayer(nn.Module):
+    """
+    The final layer of DiT.
+    """
+    def __init__(self, hidden_size, patch_size, out_channels):
+        super().__init__()
+        self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(hidden_size, patch_size * patch_size * out_channels, bias=True)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 2 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, c):
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        x = modulate(self.norm_final(x), shift, scale)
+        x = self.linear(x)
+        return x
+
+
+class CDiT(nn.Module):
+    """
+    Diffusion model with a Transformer backbone.
+    """
+    def __init__(
+        self,
+        input_size=32,
+        context_size=2,
+        patch_size=2,
+        in_channels=4,
+        hidden_size=1152,
+        depth=28,
+        num_heads=16,
+        mlp_ratio=4.0,
+        learn_sigma=True,
+    ):
+        super().__init__()
+        self.context_size = context_size
+        self.learn_sigma = learn_sigma
+        self.in_channels = in_channels
+        self.out_channels = in_channels * 2 if learn_sigma else in_channels
+        self.patch_size = patch_size
+        self.num_heads = num_heads
+        self.x_embedder = PatchEmbed(input_size, patch_size, in_channels, hidden_size, bias=True)
+        self.t_embedder = TimeStepEmbedder(hidden_size)
+        self.y_embedder = ActionStepEmbedder(hidden_size)
+        num_patches = self.x_embedder.num_patches
+        self.pos_embed = nn.Parameter(torch.zeros(self.context_size + 1, num_patches, hidden_size), requires_grad=True) # for context and for predicted frame
+        self.blocks = nn.ModuleList([CDiTBlock(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)])
+        self.final_layer = FinalLayer(hidden_size, patch_size, self.out_channels)
+        self.time_embedder = TimeStepEmbedder(hidden_size)
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Initialize transformer layers:
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+
+        # Initialize (and freeze) pos_embed by sin-cos embedding:
+        nn.init.normal_(self.pos_embed, std=0.02)
+
+        # Initialize patch_embed like nn.Linear (instead of nn.Conv2d):
+        w = self.x_embedder.proj.weight.data
+        nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+        nn.init.constant_(self.x_embedder.proj.bias, 0)
+
+
+        # Initialize action embedding:
+        nn.init.normal_(self.y_embedder.x_emb.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.y_embedder.x_emb.mlp[2].weight, std=0.02)
+
+        nn.init.normal_(self.y_embedder.y_emb.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.y_embedder.y_emb.mlp[2].weight, std=0.02)
+
+        nn.init.normal_(self.y_embedder.angle_emb.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.y_embedder.angle_emb.mlp[2].weight, std=0.02)
+
+        # Initialize timestep embedding MLP:
+        nn.init.normal_(self.t_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.t_embedder.mlp[2].weight, std=0.02)
+        
+        nn.init.normal_(self.time_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.time_embedder.mlp[2].weight, std=0.02)
+            
+        # Zero-out adaLN modulation layers in DiT blocks:
+        for block in self.blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+
+        # Zero-out output layers:
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+
+    def unpatchify(self, x):
+        """
+        x: (N, T, patch_size**2 * C)
+        imgs: (N, H, W, C)
+        """
+        c = self.out_channels
+        p = self.x_embedder.patch_size[0]
+        h = w = int(x.shape[1] ** 0.5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, c))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], c, h * p, h * p))
+        return imgs
+
+    def forward(self, x, t, y, x_cond, rel_t):
+        """
+        Forward pass of DiT.
+        x: (N, C, H, W) tensor of spatial inputs (images or latent representations of images)
+        t: (N,) tensor of diffusion timesteps
+        y: (N,) tensor of class labels
+        """
+        x = self.x_embedder(x) + self.pos_embed[self.context_size:]
+        x_cond = self.x_embedder(x_cond.flatten(0, 1)).unflatten(0, (x_cond.shape[0], x_cond.shape[1])) + self.pos_embed[:self.context_size]  # (N, T, D), where T = H * W / patch_size ** 2.flatten(1, 2)
+        x_cond = x_cond.flatten(1, 2)
+        t = self.t_embedder(t[..., None])
+        y = self.y_embedder(y) 
+        time_emb = self.time_embedder(rel_t[..., None])
+        c = t + time_emb + y # if training on unlabeled data, dont add y.
+
+        for block in self.blocks:
+            x = block(x, c, x_cond)
+        x = self.final_layer(x, c)
+        x = self.unpatchify(x)
+        return x
